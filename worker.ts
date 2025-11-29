b0cd3d8130b73de0cff9aff6defb62340aa031a9
paul1257 <3167385363@qq.com>
2025-11-29 22:45:09 +0800

    修改worker

--- worker.ts
+++ worker.ts
@@ -5,25 +5,20 @@ import { createRequestHandler } from "@react-router/cloudflare";
 declare module "react-router" {
   export interface AppLoadContext {
     cloudflare: {
-       //@ts-ignore
       env: Env;
       ctx: ExecutionContext;
     };
   }
 }
 
-const requestHandler = createRequestHandler(
-  () => import("./build/server/index.js"),
-  import.meta.env.MODE,
-);
-
 export default {
-      //@ts-ignore
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    //@ts-ignore
-    return createRequestHandler(build, import.meta.env.MODE)(request, {
+    // 运行时动态导入构建后的服务器文件
+    const serverBuild = await import("./build/server/index.js");
+    const requestHandler = createRequestHandler(serverBuild, import.meta.env.MODE);
+    
+    return requestHandler(request, {
       cloudflare: { env, ctx },
     });
   },
-   //@ts-ignore
 } satisfies ExportedHandler<Env>;
