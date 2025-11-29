8703830bacf494db9a9fc3be960fc4ec58b22f9e
paul1257 <3167385363@qq.com>
2025-11-29 23:15:05 +0800

    原来如此

--- worker.js
+++ worker.js
@@ -2,4 +2,4 @@ import { createEventHandler } from "@remix-run/cloudflare-workers";
 
 import * as build from "./build/server/index.js";
 
-addEventListener("fetch", createEventHandler({ build }));
+addEventListener("fetch", createEventHandler({ build }))
