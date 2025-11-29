a9890d4b7b508a36b205db539033a05c1eca8195
paul1257 <3167385363@qq.com>
2025-11-29 23:05:11 +0800

    修改worker

--- worker.ts
+++ worker.ts
@@ -1,24 +1,5 @@
-/// <reference types="@cloudflare/workers-types" />
+import { createEventHandler } from "@remix-run/cloudflare-workers";
 
-import { createRequestHandler } from "@react-router/cloudflare";
+import * as build from "../build";
 
-declare module "react-router" {
-  export interface AppLoadContext {
-    cloudflare: {
-      env: Env;
-      ctx: ExecutionContext;
-    };
-  }
-}
-
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    // 运行时动态导入构建后的服务器文件
-    const serverBuild = await import("./build/server/index.js");
-    const requestHandler = createRequestHandler(serverBuild, import.meta.env.MODE);
-    
-    return requestHandler(request, {
-      cloudflare: { env, ctx },
-    });
-  },
-} satisfies ExportedHandler<Env>;
+addEventListener("fetch", createEventHandler({ build }));
